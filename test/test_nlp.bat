24272af4f5f8e17c599a99d0a9dfbd6a11c73f25
Nghia Doan <nghia71@gmail.com>
2021-01-22 13:56:10 -0500

    W4

--- test/test_nlp.bat
+++ test/test_nlp.bat
@@ -1,1 +1,1 @@
-curl -w '\n' -X POST %1 -H "Content-Type: application/json" -H "Accept: application/json" -d @%2 | python -m json.tool
+curl -w "\r\n" -X POST %1 -H "Content-Type: application/json" -H "Accept: application/json" -d @%2 | python -m json.tool
