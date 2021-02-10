fb8808782accd91d32bd9b3062b6b9bc3369ab2d
Nghia Doan <>
2021-02-10 10:40:12 -0500

    Update pdf file name.

--- test/test_tika.sh
+++ test/test_tika.sh
@@ -2,4 +2,4 @@
 
 echo 'Example: ./test_tika.sh http://localhost:9998/tika ../import/salmon_anatomy_vocabulary_list.pdf'
 
-curl -X PUT --data-binary @$2 -H "Content-Type: application/pdf" $1
+curl -X PUT --data-binary @$2 -H "Content-Type: application/pdf" "$1"
